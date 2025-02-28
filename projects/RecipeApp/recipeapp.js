63067b50d9f4c033c804bd4d16e80f84b432477b
ShantanuCog <shantanu.awasthi@cognizant.com>
2025-02-28 13:16:07 +0000

    stuff

--- projects/RecipeApp/recipeapp.js
+++ projects/RecipeApp/recipeapp.js
@@ -1,4 +1,12 @@
-// Initialise the input box and the recipe list
-document.querySelector('#push').onclick = function() {
-    
-}
+// Initial References
+let result = document.getElementById("result");
+let searchButton = document.getElementById("search-button");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="; // URL for the API to filter by meal name
+
+let userInput = document.getElementById("user-input").value; // User input for the search bar
+
+fetch(url + "pizza")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    });
