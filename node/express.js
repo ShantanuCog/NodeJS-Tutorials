657a53af4ace6dfd424da19b51494d3fe7e5d84f
ShantanuCog <shantanu.awasthi@cognizant.com>
2025-02-28 15:09:27 +0000

    node server

---
+++ node/express.js
@@ -0,0 +1,28 @@
+// Import the Express package
+const express = require('express');
+
+// Create an Express application
+const app = express();
+
+// Define the port where the server will listen
+const port = 3000;
+
+// Define a route for the root URL ("/")
+// When a GET request is made to '/', this function is triggered
+app.get('/', (req, res) => {
+  // Respond with a simple message
+  res.send('Hello, Node.js World!');
+});
+
+// An additional route to demonstrate asynchronous behavior
+app.get('/async', (req, res) => {
+  // Simulate an asynchronous operation using setTimeout
+  setTimeout(() => {
+    res.send('This response was delayed by 1 second, showing non-blocking behavior!');
+  }, 1000);
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
