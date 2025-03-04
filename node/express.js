2e79af83ab707caff83a35ded200378b13548455
ShantanuCog <shantanu.awasthi@cognizant.com>
2025-03-04 15:05:24 +0000

    added hmtl file, not yet connected to express server but intend to connect it

--- node/express.js
+++ node/express.js
@@ -8,15 +8,21 @@ const app = express();
 const port = 3000;
 
 // Define a route for the root URL ("/")
-// When a GET request is made to '/', this function is triggered
-app.get('/', (req, res) => {
+// When a GET request is made to path '/', this function is triggered
+app.get('/', (req, res) => {          //  "req" and "res" are the callback objects, request and response
   // Respond with a simple message
   res.send('Hello, Node.js World!');
 });
 
+// (req, res) can be written as a function:
+// (req, res) => {          //  "req" and "res" are the callback objects, request and response
+//   // Respond with a simple message
+//   res.send('Hello, Node.js World!');
+// };
+
 // An additional route to demonstrate asynchronous behavior
 app.get('/async', (req, res) => {
-  // Simulate an asynchronous operation using setTimeout
+  // Simulate an asynchronous operation using setTimeout.
   setTimeout(() => {
     res.send('This response was delayed by 1 second, showing non-blocking behavior!');
   }, 1000);
