3db9f4cc5cfcb8ee5400c9b291a7a029d3caeb9f
ShantanuCog <shantanu.awasthi@cognizant.com>
2025-03-05 14:37:16 +0000

    emitter function added to express.js

--- node/express.js
+++ node/express.js
@@ -3,19 +3,37 @@
 // Import the Express package
 const express = require('express');
 const path = require('path');
+const eventEmitter = require('events');
 
 // Create an Express application
 const app = express();
 app.use(express.json());  // uses the specified middleware to parse JSON data  
 
+const myEmitter = new eventEmitter(); // instantiating an instance of emitter
+
 // Define the port where the server will listen
 const port = 3000;
+
+// Event listener for 'userAction' event
+myEmitter.on('userAction', (action) => {
+  console.log(`User performed action: ${action}`);
+});
 
 // Define a route for the root URL ("/")
 // When a GET request is made to path '/', this function is triggered
 app.get('/', (req, res) => {          //  "req" and "res" are the callback objects, request and response
+  
+  // accessing "/" (the root) is the action event, which will trigger the "userAction" event
+  myEmitter.emit("userAction", "/");
+
   // Respond with a simple message
   res.send('Hello, Node.js World!');
+
+  // 1) The user accesses the root URL ("/")
+  // 2) The server triggers the 'userAction' event with the action "/"
+  // 3) The server responds with a message "User performed action: /" to console
+  // 4) The server responds with a message "Hello, Node.js World!" to the client on web browser
+
 });
 
 // (req, res) can be written as a function:
